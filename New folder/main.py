7d3919a05c5fb881be3a49ad4f85ec11c1729616
sakshamratan <115889897+sakshamratan@users.noreply.github.com>
2023-02-05 19:57:15 +0530

    Update main.py

--- New folder/main.py
+++ New folder/main.py
@@ -7,34 +7,38 @@ from PIL import Image
 
 
 def experiment(annotation_file, captioner, transforms, outputs):
-    '''
-        Function to perform the desired experiments
-        Arguments:
-        annotation_file: Path to annotation file
-        captioner: The image captioner
-        transforms: List of transformation classes
-        outputs: Path of the output folder to store the images
-    '''
+    dataset = Dataset(annotation_file)
+    download = Download()
 
-    #Create the instances of the dataset, download
+#Print image names and their captions from annotation file using dataset object
+    data = dataset.get_data()
+    for i, item in enumerate(data):
+        print(f'{i + 1}. Image name: {item["image_name"]} Caption: {item["caption"]}')
 
+#Download images to ./data/imgs/ folder using download object
+    download.download(data)
 
-    #Print image names and their captions from annotation file using dataset object
-
-
-    #Download images to ./data/imgs/ folder using download object
-
-
-    #Transform the required image (roll number mod 10) and save it seperately
-
-
-    #Get the predictions from the captioner for the above saved transformed image  
-
-
+#Transform the required image (roll number mod 10) and save it seperately
+    for i, item in enumerate(data):
+        if (i+1) % 10 == 0:
+            img = Image.open(f'./data/imgs/{item["image_name"]}')
+            for transform in transforms:
+                img = transform(img)
+                img.save(f'./data/imgs/{item["image_name"]}_transformed.jpg')
+        
+#Get the predictions from the captioner for the above saved transformed image
+    for i, item in enumerate(data):
+        if (i+1) % 10 == 0:
+            img_path = f'./data/imgs/{item["image_name"]}_transformed.jpg'
+            pred = captioner.predict(img_path)
+            print(f'Caption for transformed image: {pred}')
+        
 def main():
     captioner = ImageCaptioningModel()
     experiment('./data/annotations.jsonl', captioner, [FlipImage(), BlurImage(1)], None) # Sample arguments to call experiment()
+if name == 'main':
+main()
 
 
-if __name__ == '__main__':
-    main()
+
+
